6151c67d2f13da118dc914648a354d6005baf99e
Oleg Kozlovskui <kozlovskuioleg@gmail.com>
2018-05-07 01:45:07 +0300

    change README

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ class App extends Component {
   render() {
     return (
       <Provider store={store}>
-        <Router>
+        <Router basename="/react-dj-player/">
           <Fragment>
             <Header />
             <Route exact path="/" component={Home} />
